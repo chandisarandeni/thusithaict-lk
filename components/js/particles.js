c53c22892571fb4039be8b3b07e5187003a5ae60
Chandisa Randeni <chandisarandeni2003@gmail.com>
2025-03-13 23:33:57 +0530

    Animation Canvas z-axis Changed and Navbar Docked to Top

--- components/js/particles.js
+++ components/js/particles.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("canvas1");
+const canvas = document.getElementById("animationCanvas");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
